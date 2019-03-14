cff84e0cce9e4eae0d9b1d305cb0fb9d7ecb63ce
krystianwieczorek <krystian.adam.wieczorek@gmail.com>
2019-03-14 12:58:16 +0100

    Added solution for exercise 2

--- Program.cs
+++ Program.cs
@@ -5,24 +5,27 @@
 namespace PO_lab1
 {
     //Zad_1
-   public class Samochod {
+    public class Samochod
+    {
         private string marka, model;
         private int ile_drzwi;
         private double pojemnosc_silnika, srednie_spalanie;
         private static int ilosc_samochodow = 0;
 
         //konstruktor domyslny:
-        public Samochod() {
+        public Samochod()
+        {
             marka = "nieznana marka";
             model = "nieznany model";
             ile_drzwi = 0;
             pojemnosc_silnika = 0.0;
             srednie_spalanie = 0.0;
-            ilosc_samochodow = ilosc_samochodow + 1;
+            //ilosc_samochodow = ilosc_samochodow + 1;
         }
 
         //konstruktor parametryczny:
-        public Samochod(string _marka, string _model, int _ile_drzwi, double _pojemnosc_silnika, double _srednie_spalanie ) {
+        public Samochod(string _marka, string _model, int _ile_drzwi, double _pojemnosc_silnika, double _srednie_spalanie)
+        {
             marka = _marka;
             model = _model;
             ile_drzwi = _ile_drzwi;
@@ -32,16 +35,19 @@ namespace PO_lab1
         }
 
         //metody klasy:
-        private double ObliczSpalanie(double dlugoscTrasy) {
+        private double ObliczSpalanie(double dlugoscTrasy)
+        {
             return (srednie_spalanie * dlugoscTrasy) / 100;
         }
 
-        public double ObliczKosztPrzejazdu(double dlugoscTrasy, double cenaPaliwa) {
+        public double ObliczKosztPrzejazdu(double dlugoscTrasy, double cenaPaliwa)
+        {
             double x = ObliczSpalanie(dlugoscTrasy);
             return x * cenaPaliwa;
         }
 
-        public void WypiszInfo() {
+        public void WypiszInfo()
+        {
             Console.WriteLine("Marka: " + marka);
             Console.WriteLine("Model: " + model);
             Console.WriteLine("Ilość Drzwi: " + ile_drzwi);
@@ -49,15 +55,16 @@ namespace PO_lab1
             Console.WriteLine("Srednie Spalanie: " + srednie_spalanie);
         }
 
-        public static void WypiszIloscSamochodow() {
+        public static void WypiszIloscSamochodow()
+        {
             Console.WriteLine("Ilość Samochodów: " + ilosc_samochodow);
         }
 
         // wlasciwosci klasy:
         public string Marka
         {
-            get {return marka;}
-            set { marka = value;}
+            get { return marka; }
+            set { marka = value; }
         }
 
         public string Model
@@ -83,31 +90,124 @@ namespace PO_lab1
             get { return pojemnosc_silnika; }
             set { pojemnosc_silnika = value; }
         }
-  
+
 
     }
 
+    //Zad_2
+    class Garaz
+    {
+        private string adres;
+        private int pojemnosc;
+        private int liczbaSamochodow = 0;
+        private Samochod[] samochody;
 
+        public string Adres
+        {
 
-    class Program{
-        static void Main(string[] args){
+            get { return adres; }
+            set { adres = value; }
 
-            Samochod s1 = new Samochod();
-            s1.WypiszInfo();
-            s1.Marka = "Fiat";
-            s1.Model = "126p";
-            s1.Ile_drzwi = 2;
-            s1.Pojemnosc_silnika = 650;
-            s1.Srednie_spalanie = 6.0;
-            s1.WypiszInfo();
-            Samochod s2 = new Samochod("Syrena", "105", 2, 800, 7.6);
-            s2.WypiszInfo();
-            double kosztPrzejazdu = s2.ObliczKosztPrzejazdu(30.5, 4.85);
-            Console.WriteLine("Koszt przejazdu: " + kosztPrzejazdu);
-            Samochod.WypiszIloscSamochodow();
-            Console.ReadKey();
+        }
+
+        public int Pojemnosc
+        {
+
+            get { return pojemnosc; }
+            set { pojemnosc = value; samochody = new Samochod[pojemnosc]; }
+        }
+
+        public Garaz()
+        {
+
+            adres = "nieznany";
+            pojemnosc = 0;
+            samochody = null;
+        }
+
+        public Garaz(string _adres, int _pojemnosc)
+        {
+
+            adres = _adres;
+            pojemnosc = _pojemnosc;
+            samochody = new Samochod[_pojemnosc];
+        }
+
+        public void WprowadzSamochod(Samochod samochod)
+        {
+
+            if (pojemnosc == liczbaSamochodow)
+            {
+                Console.WriteLine("garaż jest pełny");
+            }
+
+            else
+            {
+                samochody[liczbaSamochodow] = samochod;
+                liczbaSamochodow++;
+            }
+
+        }
+
+        public Samochod WyprowadzSamochod()
+        {
+
+            if (liczbaSamochodow == 0)
+            {
+                Console.WriteLine("Garaz jest pusty");
+            }
+
+            else
+            {
+                liczbaSamochodow--;
+                samochody[liczbaSamochodow] = null;
+
 
             }
+            return samochody[liczbaSamochodow];
         }
+
+        public void WypiszInfo()
+        {
+
+            Console.WriteLine("Adres Garażu:" + adres);
+            Console.WriteLine("Pojemność Garażu:" + pojemnosc);
+            Console.WriteLine("Liczba Samochodów w garażu:" + liczbaSamochodow);
+
+            for (int i = 0; i < liczbaSamochodow; i++)
+            {
+                samochody[i].WypiszInfo();
+            }
+
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Samochod s1 = new Samochod("Fiat", "126p", 2, 650, 6.0);
+            Samochod s2 = new Samochod("Syrena", "105", 2, 800, 7.6);
+            Garaz g1 = new Garaz();
+            g1.Adres = "ul. Garażowa 1";
+            g1.Pojemnosc = 1;
+            Garaz g2 = new Garaz("ul. Garażowa 2", 2);
+            g1.WprowadzSamochod(s1);
+            g1.WypiszInfo();
+            g1.WprowadzSamochod(s2);
+            g2.WprowadzSamochod(s2);
+            g2.WprowadzSamochod(s1);
+            g2.WypiszInfo();
+            g2.WyprowadzSamochod();
+            g2.WypiszInfo();
+            g2.WyprowadzSamochod();
+            g2.WyprowadzSamochod();
+            Console.ReadKey();
+
+        }
+    }
 }
 
